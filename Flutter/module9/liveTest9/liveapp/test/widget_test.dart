e110c41e6dee34630f65557b95b6c66de6a973c9
Sayed Hossain Jobayer <62154398+BeingCyborg@users.noreply.github.com>
2023-07-10 23:44:43 +0600

    1

--- Flutter/module9/liveTest9/liveapp/test/widget_test.dart
+++ Flutter/module9/liveTest9/liveapp/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:liveapp/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget( RecipeListWidget());
+    await tester.pumpWidget(const RecipeListWidget());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
